0db03218fa8bf80223bb4903a4a0afe34ef6a565
Alex <Aleksej91@mail.ru>
2026-05-25 11:15:50 +0300

    Интегрировал search команду в main

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -2,12 +2,18 @@
 const version = '0.0.1'; // Add this line
 
 void main(List<String> arguments) {
-  if (arguments.isEmpty) {
-    print('Hello, Dart!');
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    // Add this new block:
+    print('Search command recognized!');
+  } else {
+    printUsage();
   }
 }
+
 
 void printUsage() { // Add this new function
   print(
